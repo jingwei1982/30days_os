0d03fe913bfa4d7c47f5e7aabed7cec523f89b6b
jing-ubuntu <skyweijing@gmail.com>
2022-02-03 18:23:14 +0800

    add day03h

--- day03/h/haribote.asm
+++ day03/h/haribote.asm
@@ -1,19 +1,3 @@
-;set cusror
-	push 	DX
-	mov dx,0100h
-	call set_curser_pos
-	pop DX
-
-; 打印L以确认进入了haribote。
-	push	ax
-	push	bx
-	mov	ah,	0eh
-	mov	al,	'L'
-	mov	bl,	0fh
-	int	10h
-	pop	bx
-	pop	ax
-
 ; haribote-os boot asm
 ; TAB=4
 
@@ -29,9 +13,9 @@ SCRNX	EQU		0x0ff4			; 分辨率X
 SCRNY	EQU		0x0ff6			; 分辨率Y
 VRAM	EQU		0x0ff8			; 图像缓冲区的起始地址
 
-		; MOV		AL,0x13			; VGA显卡，320x200x8bit
-		; MOV		AH,0x00
-		; INT		0x10
+		MOV		AL,0x13			; VGA显卡，320x200x8bit
+		MOV		AH,0x00
+		INT		0x10
 		MOV		BYTE [VMODE],8	; 屏幕的模式（参考C语言的引用）
 		MOV		WORD [SCRNX],320
 		MOV		WORD [SCRNY],200
@@ -47,16 +31,3 @@ VRAM	EQU		0x0ff8			; 图像缓冲区的起始地址
     fin:
     HLT
     JMP fin
-
-
-;=======	set focus
-;usage: step1, mov dx,0000h, step2, call set_cursor_pos
-set_curser_pos:
-		push 	AX
-		push 	BX
-		mov		AX,	0200h
-		mov		BX,	0000h
-		int		10h
-		pop 	BX
-		pop		AX
-		ret 
