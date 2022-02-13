96cbc102bd98de7eef774e5466b40ac4f502e3a3
jing-ubuntu <skyweijing@gmail.com>
2022-02-13 19:45:02 +0800

    add day06c and day06d

---
+++ day06/c/kernel/bootpack.c
@@ -0,0 +1,27 @@
+#include "bootpack.h"
+#include <stdio.h>
+
+void HariMain(void)
+{
+	struct BOOTINFO *binfo=(struct BOOTINFO *)0x0ff0;;
+	char mcursor[256];
+	int mx,my;
+
+	init_gdtidt();
+	init_palette();/* 设定调色板 */
+	init_screen(binfo->vram,binfo->scrnx,binfo->scrny);
+
+	// char s[40];
+	// sprintf(s,"scrnx=%d",binfo->scrnx);
+	// putfonts8_asc(binfo->vram,binfo->scrnx,16,64,COL8_FFFFFF,s);
+	mx=(binfo->scrnx-16)/2;
+	my=(binfo->scrny-28-16)/2;
+	
+	init_mouse_cursor8(mcursor,COL8_008484);
+	putblock8_8(binfo->vram,binfo->scrnx,16,16,mx,my,mcursor,16);
+
+	for (;;) {
+		io_hlt();
+	}
+}
+
