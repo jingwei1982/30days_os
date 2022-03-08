ff08a8acee2c7c23585d9918b9742c23d3cae5a6
jing-ubuntu <skyweijing@gmail.com>
2022-03-08 20:55:23 +0800

    add day20d

---
+++ day20/d/api/hlt.asm
@@ -0,0 +1,12 @@
+[bits 32]
+     mov al,'H'
+     call 2*8:0x30541
+     mov al,'E'
+     call 2*8:0x30541
+     mov al,'L'
+     call 2*8:0x30541
+     mov al,'L'
+     call 2*8:0x30541
+     mov al,'O'
+     call 2*8:0x30541
+     retf
